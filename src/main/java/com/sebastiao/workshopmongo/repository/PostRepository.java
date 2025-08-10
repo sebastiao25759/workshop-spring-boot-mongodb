0508aa68ec4f6693b3b00c82ec8bac31cc7d7b45
sebastiao25759 <sebastiao.rodrigues.25759@aluno.facape.br>
2025-08-10 20:16:38 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/sebastiao/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.sebastiao.workshopmongo.repository;
+
+import com.sebastiao.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
