13a4c2a7f8049ecac8abda32ed8164de2834d895
sebastiao25759 <sebastiao.rodrigues.25759@aluno.facape.br>
2025-08-10 14:14:15 -0300

    Obtendo um usuario por id

--- src/main/java/com/sebastiao/workshopmongo/services/UserService.java
+++ src/main/java/com/sebastiao/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.sebastiao.workshopmongo.services;
 
 import com.sebastiao.workshopmongo.domain.User;
 import com.sebastiao.workshopmongo.repository.UserRepository;
+import com.sebastiao.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,4 +18,10 @@ public class UserService {
     public List<User> findAll() {
         return repository.findAll();
     }
+
+    public User findById(String id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
 }
---
+++ src/main/java/com/sebastiao/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package com.sebastiao.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+    public ObjectNotFoundException(String msg) {
+        super(msg);
+    }
+}
