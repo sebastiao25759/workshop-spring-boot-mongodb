d85ac23c97c73694cb744c830f64b62cb82a138b
sebastiao25759 <sebastiao.rodrigues.25759@aluno.facape.br>
2025-08-10 12:50:01 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/sebastiao/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.sebastiao.workshopmongo.repository;
+
+import com.sebastiao.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
